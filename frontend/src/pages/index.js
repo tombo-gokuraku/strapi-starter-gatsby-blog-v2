dbdb8ec83f036ff2022eac33ad20370a7860612c
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-09-30 12:00:40 +0900

    feat: Fetch draft articles at index page in preview build.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,62 +1,93 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import ArticlesComponent from "../components/articles"
 
 import "../assets/css/main.css"
 
-const IndexPage = () => (
-  <Layout>
-    <StaticQuery
-      query={graphql`
-        query {
-          strapiHomepage {
-            Hero {
-              HeroText
+const query = graphql`
+  query {
+    strapiHomepage {
+      Hero {
+        HeroText
+      }
+    }
+    preview: allStrapiArticle {
+      edges {
+        node {
+          strapiId
+          slug
+          title
+          category {
+            name
+          }
+          image {
+            childImageSharp {
+              fixed(width: 800, height: 500) {
+                src
+              }
             }
           }
-          allStrapiArticle(filter: {status: {eq: "published"}}) {
-            edges {
-              node {
-                strapiId
-                slug
-                title
-                category {
-                  name
-                }
-                image {
-                  childImageSharp {
-                      fixed(width: 800, height: 500) {
-                      	src
-                      }
-                  }
-                }
-                user {
-                  username
-                  image {
-                    childImageSharp {
-                        fixed(width: 30, height: 30) {
-                        	src
-                        }
-                    }
-                  }
+          user {
+            username
+            image {
+              childImageSharp {
+                fixed(width: 30, height: 30) {
+                  src
                 }
               }
             }
           }
         }
-      `}
-      render={data => (
-        <div className="uk-section">
-          <div className="uk-container uk-container-large">
-            <h1>{data.strapiHomepage.Hero.HeroText}</h1>
-            <ArticlesComponent articles={data.allStrapiArticle.edges} />
-          </div>
-        </div>
-      )}
-    />
-  </Layout>
-)
+      }
+    }
+    deploy: allStrapiArticle(filter: { status: { eq: "published" } }) {
+      edges {
+        node {
+          strapiId
+          slug
+          title
+          category {
+            name
+          }
+          image {
+            childImageSharp {
+              fixed(width: 800, height: 500) {
+                src
+              }
+            }
+          }
+          user {
+            username
+            image {
+              childImageSharp {
+                fixed(width: 30, height: 30) {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
+const IndexPage = () => {
+  const data = useStaticQuery(query)
+  const articles = process.env.IS_PREVIEW
+    ? data.preview.edges
+    : data.deploy.edges
+  return (
+    <Layout>
+      <div className="uk-section">
+        <div className="uk-container uk-container-large">
+          <h1>{data.strapiHomepage.Hero.HeroText}</h1>
+          <ArticlesComponent articles={articles} />
+        </div>
+      </div>
+    </Layout>
+  )
+}
 export default IndexPage
